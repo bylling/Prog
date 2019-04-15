723008d2167af9a6a9e865718e44e1da19d3574a
bylling <201507956@post.au.dk>
2019-04-16 00:00:43 +0200

    Version 16-4-19

---
+++ Numeriske_Metoder/ex_eigen/eigenfunctions.h
@@ -0,0 +1,7 @@
+void vector_print(const char* s, gsl_vector* v);
+void matrix_print(const char* s, gsl_matrix* A);
+int jacobi(gsl_matrix* A, gsl_vector* e, gsl_matrix* V);
+int jacobi_mod_ev(gsl_matrix* A, gsl_vector* e, gsl_matrix* V, int eigennr);
+int jacobi_mod_highev(gsl_matrix* A, gsl_vector* e, gsl_matrix* V, int eigennr);
+int jacobi_versc(gsl_matrix* A, gsl_vector* e, gsl_matrix* V);
+int find_max_in_row(gsl_matrix* A, int r);
